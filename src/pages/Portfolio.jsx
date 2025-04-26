8169bfaf43e407e75141fe637b961fd3b2954d8f
MB DEV <mbdev.web13@gmail.com>
2025-04-26 15:30:27 +0200

    ajout du alt au images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,36 +9,42 @@ const projects = [
     subtitle: "Site de vente de produits frais en ligne",
     tech: "Site réalisé avec : PHP et MySQL",
     img: "/images/portfolio/fresh-food.jpg",
+    alt: "Image du site Fresh Food",
   },
   {
     title: "Restaurant Akira",
     subtitle: "Site de vente de produits frais en ligne",
     tech: "Site réalisé avec WordPress",
     img: "/images/portfolio/restaurant-japonais.jpg",
+    alt: "Image du site Restaurant Akira",
   },
   {
     title: "Espace bien-être",
     subtitle: "Site de vente de produits frais en ligne",
     tech: "Site réalisé avec LARAVEL",
     img: "/images/portfolio/espace-bien-etre.jpg",
+    alt: "Image du site Espace bien-être",
   },
   {
     title: "SEO",
     subtitle: "Amélioration du référencement d’un site e-commerce",
     tech: "Utilisation des outils SEO",
     img: "/images/portfolio/seo.jpg",
+    alt: "Image représentant le SEO",
   },
   {
     title: "Création d'une API",
     subtitle: "Création d’une API RESTFUL publique",
     tech: "PHP - SYMFONY",
     img: "/images/portfolio/coder.jpg",
+    alt: "Image de création d'une API",
   },
   {
     title: "Maquette d'un site web",
     subtitle: "Création d’un prototype d’un site",
     tech: "Réalisé avec FIGMA",
     img: "/images/portfolio/screens.jpg",
+    alt: "Image d'une maquette de site web",
   },
 ];
 
@@ -65,6 +71,7 @@ const Portfolio = () => {
                   <Card.Img
                     variant="top"
                     src={project.img}
+                    alt={project.alt}
                     className="card-img-top"
                   />
                   <Card.Body className="text-center">
